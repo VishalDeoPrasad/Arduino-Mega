7fce5a4ad00cf81e09d95d47f78ec6f160dc2d48
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-01-13 02:52:27 +0530

    light cup sensor

---
+++ Light_Cup_Sensor.ino
@@ -0,0 +1,23 @@
+int switchPin = 42;
+int ledPin = 40;
+int state;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(switchPin, INPUT_PULLUP);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  state = digitalRead(switchPin);
+  Serial.print("Sensor Current Value : ");
+  Serial.print(state);
+  if (state == 1){
+    digitalWrite(ledPin, HIGH);
+    Serial.println("\tled ON");
+  }
+  else{
+    digitalWrite(ledPin, LOW);
+    Serial.println("\tled OFF");
+  }
+}
